e1587ceb874dc4e8861c0494e3daa53c0fa6df40
Luke Martin <luke_martin45@yahoo.com>
2017-08-18 20:12:52 -0800

    correct import of jsoup

--- src/BaalzePageCollector.java
+++ src/BaalzePageCollector.java
@@ -1,5 +1,20 @@
-import org.jsoup.*;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.*;
+
 
 public class BaalzePageCollector {
-
+	
+	public static void main(String[] args) {
+		Document doc;
+		try {
+			doc = Jsoup.connect("http://en.wikipedia.org/").get();
+			Elements newsHeadlines = doc.select("#mp-itn b a");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
